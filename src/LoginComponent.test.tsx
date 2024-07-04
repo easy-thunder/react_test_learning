3c291b497f6c19a09863074dd6ba103b3d5f3d84
Jacob David Diehl <jakediehl17@gmail.com>
2024-07-04 09:35:21 -0600

    learning about using jest.fn() mocking login functionality and using vanilla js queries for retrieving component values

--- src/LoginComponent.test.tsx
+++ src/LoginComponent.test.tsx
@@ -1,29 +1,45 @@
-import { render, screen } from "@testing-library/react"
-import LoginComponent from "./LoginComponent"
+/* eslint-disable testing-library/no-node-access */
+import { render, screen } from "@testing-library/react";
+import LoginComponent from "./LoginComponent";
+import { act } from "react";
 
-
-
-describe('Login components test',()=>{
-
+describe('Login components test', () => {
     const loginServiceMock = {
         login: jest.fn()
+    };
+    const setTokenMock = jest.fn();
+
+    let container: HTMLElement;
+
+    function setup() {
+        container = render(
+            <LoginComponent
+                loginService={loginServiceMock}
+                setToken={setTokenMock}
+            />
+        ).container;
     }
-    const setTokenMock = jest.fn()
 
-    it('should render correctly the login component',()=>{
-        const container = render(<LoginComponent
-            loginService = {loginServiceMock}
-            setToken={setTokenMock}
-            />).container;
+    beforeEach(() => {
+        setup();
+    });
 
-            console.log(container.innerHTML)
+    it('should render correctly the login component', () => {
+        const inputs = screen.getAllByTestId('input');
 
-            const mainElement = screen.getByRole('main');
-            expect(mainElement).toBeInTheDocument();
+        expect(inputs).toHaveLength(3);
+        expect(inputs[0].getAttribute('value')).toBe('');
+        expect(inputs[1].getAttribute('value')).toBe('');
+        expect(inputs[2].getAttribute('value')).toBe('Login');
+    });
 
-    })
+    it('should render correctly - query by document query', () => {
+        const inputs = container.querySelectorAll('input');
 
-
-})
-
+        expect(inputs).toHaveLength(3);
+        expect(inputs[0].getAttribute('value')).toBe('');
+        expect(inputs[1].getAttribute('value')).toBe('');
+        expect(inputs[2].getAttribute('value')).toBe('Login');
+    });
+});
 
